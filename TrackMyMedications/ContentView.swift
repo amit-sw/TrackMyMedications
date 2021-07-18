2842ab13d731a2300cf2239657a2f0efc670e735
Amit Gupta <amit@gprof.com>
2021-07-18 14:23:39 -0700

    Initial Commit

---
+++ TrackMyMedications/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrackMyMedications
+//
+//  Created by Amit Gupta on 7/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrackMyMedications/TrackMyMedicationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackMyMedicationsApp.swift
+//  TrackMyMedications
+//
+//  Created by Amit Gupta on 7/18/21.
+//
+
+import SwiftUI
+
+@main
+struct TrackMyMedicationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
